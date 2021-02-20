5b2bc7b67cd01fa418496a7d038bbcdbaf98bd84
mohitverma6161 <utkarshmalik06@gmail.com>
2021-02-20 21:32:25 +0530

    basic routing example

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import UserComponent from './Components/User';
 import {Spinner} from 'react-bootstrap';
 
+import {BrowserRouter as Router ,Link,Switch,Route} from 'react-router-dom';
 
 
 
-const renderUserData = () => {
 
-    return this.state.allUsers.map((user) => {
-        return <UserComponent key={user.id} userDetails={user} />;
-    })
-}
+// const renderUserData = () => {
+
+//     return this.state.allUsers.map((user) => {
+//         return <UserComponent key={user.id} userDetails={user} />;
+//     })
+// }
 
 class App extends React.Component {
 
@@ -32,127 +34,167 @@ class App extends React.Component {
         }
     }
 
-    componentDidMount()
-    {
-        console.log('Inside Component Did Mount');
-        
-        fetch("https://dummyapi.io/data/api/user",{
-            method:"GET",
-            headers:{"app-id":"5fca5a3236a334a6a8f40cc7"}
-        }).then(data=>data.json())
-        .then(data=>{
-            const users=data.data;
-            this.setState({allUsers:users,currentUsers:users});
-        }
-        );
-    }
+    render(){
 
-
-    onEditUser=(id,newFirstname)=>
-    {
-        console.log(id);
-        console.log(newFirstname);
-
-        const updatedUsers=[...this.state.currentUsers];
-
-        updatedUsers.forEach((user)=>
-        {
-            if(user.id===id)
-            {
-                user.firstName=newFirstname;
-            }
-        });
-
-        return updatedUsers;
-    }
-
-
-
-
-    onDeleteUser = (id) => {
-        console.log(id);
-
-        const updatedUsers=this.state.currentUsers.filter((user)=>
-        {
-            return user.id!==id;
-        });
-
-        this.setState({currentUsers:updatedUsers});
-    }
-
-
-
-    renderUserData() {
-        return this.state.currentUsers.map((user) => {
-            return <UserComponent 
-            key={user.id} 
-            userDetails={user} 
-            onDeleteUser={(id) => this.onDeleteUser(id)}
-            onEditUser={(id,newFirstname)=>this.onEditUser(id,newFirstname)} />
-        })
-    }
-
-    onInputChange(e) {
-
-        const value = e.target.value;
-
-        this.setState({ textField: value });
-
-
-        const updatedUsers = this.state.allUsers.filter((user) => {
-            const fullName = user.firstName + " " + user.lastName;
-            return fullName.toLowerCase().startsWith(value);
-        });
-
-        this.setState({ currentUsers: updatedUsers });
-
-
-    }
-
-    onResetUsers() {
-        this.setState({ currentUsers: this.state.allUsers });
-    }
-
-
-    onSortUsers() {
-        function compare(user1, user2) {
-            const name1 = user1.firstName.toLowerCase();
-            const name2 = user2.firstName.toLowerCase();
-            if (name1 < name2) {
-                return -1;
-            }
-            if (name1 > name2) {
-                return 1;
-            }
-            return 0;
-        }
-
-        const currentUsersCopy = [...this.state.currentUsers];
-
-        currentUsersCopy.sort(compare);
-
-        console.log(currentUsersCopy);
-
-        this.setState({ currentUsers: currentUsersCopy });
-    }
-
-    render() {
-        console.log('Inside Render Menthod');
-        return (
-            <div className="App">
-                <h2>User Data</h2>
-
-                <div>
-                    <input type="text" style={{ width: "400px", height: "25px" }} placeholder="Enter Text Here" value={this.state.textField} onChange={(e) => this.onInputChange(e)} />
-                    <button onClick={() => this.onSortUsers()} style={{ border: "3px solid black", margin: "0 10px", padding: "8px 30px", cursor: "pointer" }}>Sort Users</button>
-                    <button onClick={() => this.onResetUsers()} style={{ border: "3px solid black", margin: "0 10px", padding: "8px 30px", cursor: "pointer" }}>Reset Users</button>
-
-                </div>
-
-                {(this.state.allUsers)?this.renderUserData():<Spinner animation="border" />};
+        return(
+            <Router>
+            <div>
+            <ul style={{display:"flex",justifyContent:'space-around'}} >
+            <li> <Link to='/home' > Home </Link> </li>
+            <li> <Link to='/about' > About  </Link> </li>
+            <li> <Link to='/contacts' > Contact </Link> </li>
+            </ul>
             </div>
-        )
+
+            <Switch>
+
+            <Route path='/home'>
+            <h1>We are on home page</h1>
+            </Route>
+            <Route path='/about'>
+            <h1>We are on about page</h1>
+            </Route>
+
+            <Route path='/contacts'>
+            <h1>We are on Contacts page</h1>
+            </Route>
+
+            <Route path='/'>
+            <h1>We are on top level page</h1>
+            </Route>
+
+
+            </Switch>
+            
+            </Router>
+            )
+
     }
+
+    
+
+    // componentDidMount()
+    // {
+    //     console.log('Inside Component Did Mount');
+        
+    //     fetch("https://dummyapi.io/data/api/user",{
+    //         method:"GET",
+    //         headers:{"app-id":"5fca5a3236a334a6a8f40cc7"}
+    //     }).then(data=>data.json())
+    //     .then(data=>{
+    //         const users=data.data;
+    //         this.setState({allUsers:users,currentUsers:users});
+    //     }
+    //     );
+    // }
+
+
+    // onEditUser=(id,newFirstname)=>
+    // {
+    //     console.log(id);
+    //     console.log(newFirstname);
+
+    
+    //     const updatedUsers=[...this.state.currentUsers];
+
+    //     updatedUsers.forEach((user)=>
+    //     {
+    //         if(user.id===id)
+    //         {
+    //             user.firstName=newFirstname;
+    //         }
+    //     });
+
+    //     return updatedUsers;
+    // }
+
+
+
+
+    // onDeleteUser = (id) => {
+    //     console.log(id);
+
+    //     const updatedUsers=this.state.currentUsers.filter((user)=>
+    //     {
+    //         return user.id!==id;
+    //     });
+
+    //     this.setState({currentUsers:updatedUsers});
+    // }
+
+
+
+    // renderUserData() {
+    //     return this.state.currentUsers.map((user) => {
+    //         return <UserComponent 
+    //         key={user.id} 
+    //         userDetails={user} 
+    //         onDeleteUser={(id) => this.onDeleteUser(id)}
+    //         onEditUser={(id,newFirstname)=>this.onEditUser(id,newFirstname)} />
+    //     })
+    // }
+
+    // onInputChange(e) {
+
+    //     const value = e.target.value;
+
+    //     this.setState({ textField: value });
+
+
+    //     const updatedUsers = this.state.allUsers.filter((user) => {
+    //         const fullName = user.firstName + " " + user.lastName;
+    //         return fullName.toLowerCase().startsWith(value);
+    //     });
+
+    //     this.setState({ currentUsers: updatedUsers });
+
+
+    // }
+
+    // onResetUsers() {
+    //     this.setState({ currentUsers: this.state.allUsers });
+    // }
+
+
+    // onSortUsers() {
+    //     function compare(user1, user2) {
+    //         const name1 = user1.firstName.toLowerCase();
+    //         const name2 = user2.firstName.toLowerCase();
+    //         if (name1 < name2) {
+    //             return -1;
+    //         }
+    //         if (name1 > name2) {
+    //             return 1;
+    //         }
+    //         return 0;
+    //     }
+
+    //     const currentUsersCopy = [...this.state.currentUsers];
+
+    //     currentUsersCopy.sort(compare);
+
+    //     console.log(currentUsersCopy);
+
+    //     this.setState({ currentUsers: currentUsersCopy });
+    // }
+
+    // render() {
+    //     console.log('Inside Render Menthod');
+    //     return (
+    //         <div className="App">
+    //             <h2>User Data</h2>
+
+    //             <div>
+    //                 <input type="text" style={{ width: "400px", height: "25px" }} placeholder="Enter Text Here" value={this.state.textField} onChange={(e) => this.onInputChange(e)} />
+    //                 <button onClick={() => this.onSortUsers()} style={{ border: "3px solid black", margin: "0 10px", padding: "8px 30px", cursor: "pointer" }}>Sort Users</button>
+    //                 <button onClick={() => this.onResetUsers()} style={{ border: "3px solid black", margin: "0 10px", padding: "8px 30px", cursor: "pointer" }}>Reset Users</button>
+
+    //             </div>
+
+    //             {(this.state.allUsers)?this.renderUserData():<Spinner animation="border" />};
+    //         </div>
+    //     )
+    // }
 
 }
 
