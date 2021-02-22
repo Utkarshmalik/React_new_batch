24507811770afc951346fe15cbfed9226d230c25
mohitverma6161 <utkarshmalik06@gmail.com>
2021-02-22 23:55:11 +0530

    custom hooks added

--- src/App.js
+++ src/App.js
@@ -4,42 +4,40 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FullUser from './Components/FullUser';
 
-import {BrowserRouter as Router ,Link,Switch,Route} from 'react-router-dom';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import UserList from './Components/UsersList';
 
 
 
-const App=()=> {
-    
-    
-        return (
-            <Router>
+const App = () => {
+
+
+    return (
+        <Router>
             <div className="App">
-            <ul style={{display:"flex",justifyContent:"space-around",width:"20%",border:"3px solid red",color:"white",backgroundColor:"black"}} >
-            <li><Link to="/home">Home</Link> </li>
-            <li><Link to="/users">Users</Link> </li>
-            </ul>
+                <ul style={{ display: "flex", justifyContent: "space-around", width: "20%", border: "3px solid red", color: "white", backgroundColor: "black" }} >
+                    <li><Link to="/home">Home</Link> </li>
+                    <li><Link to="/users">Users</Link> </li>
+                </ul>
+                <Switch>
+                    <Route exact path='/'>
+                        <h1>Landing Page</h1>
+                    </Route>
 
-            <Switch>
+                    <Route path='/home'>
+                        <h1>Home Page</h1>
+                    </Route>
+                    <Route exact path='/users/'>
+                        <UserList />
+                    </Route>
 
-            <Route exact path='/'>
-            <h1>Landing Page</h1>
-            </Route>
-
-            <Route path='/home'>
-            <h1>Home Page</h1>
-            </Route>
-            <Route exact path='/users/'>
-            <UserList/>
-            </Route>
-
-            <Route path='/users/:id'>
-            <FullUser/>
-            </Route>
-            </Switch>
+                    <Route path='/users/:id'>
+                        <FullUser />
+                    </Route>
+                </Switch>
             </div>
-            </Router>
-        )
+        </Router>
+    )
 }
 
 
--- src/Components/UsersList.js
+++ src/Components/UsersList.js
@@ -1,128 +1,42 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import '../App.css';
 import UserComponent from './User';
-import {Spinner} from 'react-bootstrap';
-import {BrowserRouter as Router ,Link,Switch,Route} from 'react-router-dom';
+import { Spinner } from 'react-bootstrap';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 
 
 
 
 
-const UserList=()=> {
+const UserList = () => {
 
 
 
+    const [textField, changeTextField] = useState("");
+    const [allUsers, changeAllUsers] = useState(null);
+    const [currentUsers, changeCurrentUsers] = useState(null);
 
-    const [textField,changeTextField]=useState("");
-    const [allUsers,changeAllUsers]=useState(null);
-    const [currentUsers,changeCurrentUsers]=useState(null);
 
+    useEffect(() => {
 
-    useEffect(()=>
-    {
         console.log("use Effect called");
-    fetch("https://dummyapi.io/data/api/user",{
-        method:"GET",
-        headers:{"app-id":"5fca5a3236a334a6a8f40cc7"}
-    }).then(data=>data.json())
-    .then(data=>{
-        const users=data.data;
-        changeAllUsers(users);
-        changeCurrentUsers(users);
-    });
-    },[])
-    
-const onEditUser=(id,newFirstname)=>
-{
-    console.log(id);
-    console.log(newFirstname);
+        fetch("https://dummyapi.io/data/api/user/", {
+            method: "GET",
+            headers: { "app-id": "5fca5a3236a334a6a8f40cc7" }
+        }).then(data => data.json())
+            .then(data => {
+                const users = data.data;
+                changeAllUsers(users);
+                changeCurrentUsers(users);
+            });
 
-
-    const updatedUsers=[...currentUsers];
-
-    updatedUsers.forEach((user)=>
-    {
-        if(user.id===id)
-        {
-            user.firstName=newFirstname;
+        return () => {
+            console.log("COmponent will unmount");
         }
-    });
+    }, [])
 
-    return updatedUsers;
-}
-
-
-
-
-const onDeleteUser = (id) => {
-    console.log(id);
-
-    const updatedUsers=currentUsers.filter((user)=>
-    {
-        return user.id!==id;
-    });
-
-    changeAllUsers(updatedUsers);
-}
-
-
-
-const renderUserData=()=> {
-
-    console.log(currentUsers);
-    return currentUsers.map((user) => {
-        return <UserComponent 
-        key={user.id} 
-        userDetails={user} 
-        onDeleteUser={(id) => onDeleteUser(id)}
-        onEditUser={(id,newFirstname)=>onEditUser(id,newFirstname)} />
-    })
-}
-
-const onInputChange=(e)=> {
-
-    const value = e.target.value;
-
-    changeTextField(value);
-
-
-    const updatedUsers = allUsers.filter((user) => {
-        const fullName = user.firstName + " " + user.lastName;
-        return fullName.toLowerCase().startsWith(value);
-    });
-
-    changeCurrentUsers(updatedUsers);
-
-}
-
-const onResetUsers=()=> {
-    changeCurrentUsers(allUsers);
-}
-
-
-const onSortUsers=()=>{
-    function compare(user1, user2) {
-        const name1 = user1.firstName.toLowerCase();
-        const name2 = user2.firstName.toLowerCase();
-        if (name1 < name2) {
-            return -1;
-        }
-        if (name1 > name2) {
-            return 1;
-        }
-        return 0;
-    }
-
-    const currentUsersCopy = [...currentUsers];
-
-    currentUsersCopy.sort(compare);
-
-    console.log(currentUsersCopy);
-
-    changeCurrentUsers(currentUsersCopy);
-
-}
-
+    const {onSortUsers,onResetUsers,renderUserData,onInputChange}=useUserListHook(allUsers,changeAllUsers,currentUsers,changeCurrentUsers,changeTextField);
+  
 
     return (
         <div className="App">
@@ -136,12 +50,103 @@ const onSortUsers=()=>{
             </div>
 
             {
-                (currentUsers)?renderUserData():<Spinner animation="border" />
-            
+                (currentUsers) ? renderUserData() : <Spinner animation="border" />
+
             };
         </div>
     )
+}
 
+const useUserListHook=(allUsers,changeAllUsers,currentUsers,changeCurrentUsers,changeTextField)=>
+{
+    const onEditUser = (id, newFirstname) => {
+        console.log(id);
+        console.log(newFirstname);
+
+
+        const updatedUsers = [...currentUsers];
+
+        updatedUsers.forEach((user) => {
+            if (user.id === id) {
+                user.firstName = newFirstname;
+            }
+        });
+
+        return updatedUsers;
+    }
+
+    const onDeleteUser = (id) => {
+        console.log(id);
+
+        const updatedUsers = currentUsers.filter((user) => {
+            return user.id !== id;
+        });
+
+        changeAllUsers(updatedUsers);
+    }
+
+
+
+    const renderUserData = () => {
+
+        console.log(currentUsers);
+        return currentUsers.map((user) => {
+            return <UserComponent
+                key={user.id}
+                userDetails={user}
+                onDeleteUser={(id) => onDeleteUser(id)}
+                onEditUser={(id, newFirstname) => onEditUser(id, newFirstname)} />
+        })
+    }
+
+
+
+    const onInputChange = (e) => {
+
+        const value = e.target.value;
+
+        changeTextField(value);
+
+
+        const updatedUsers = allUsers.filter((user) => {
+            const fullName = user.firstName + " " + user.lastName;
+            return fullName.toLowerCase().startsWith(value);
+        });
+
+        changeCurrentUsers(updatedUsers);
+
+    }
+
+    const onResetUsers = () => {
+        changeCurrentUsers(allUsers);
+    }
+
+
+    const onSortUsers = () => {
+        function compare(user1, user2) {
+            const name1 = user1.firstName.toLowerCase();
+            const name2 = user2.firstName.toLowerCase();
+            if (name1 < name2) {
+                return -1;
+            }
+            if (name1 > name2) {
+                return 1;
+            }
+            return 0;
+        }
+
+        const currentUsersCopy = [...currentUsers];
+
+        currentUsersCopy.sort(compare);
+
+        console.log(currentUsersCopy);
+
+        changeCurrentUsers(currentUsersCopy);
+
+    }
+
+
+    return {onSortUsers,onResetUsers,renderUserData,onInputChange};
 }
 
 
