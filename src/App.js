0d1b5681a410c8a6015bd37acbb60499e0cbb00b
mohitverma6161 <utkarshmalik06@gmail.com>
2021-03-13 22:26:18 +0530

    Advacned Topics list added

--- src/App.js
+++ src/App.js
@@ -1,54 +1,90 @@
 import logo from './logo.svg';
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import HomePage from './Components/HomePage';
 import ThemeContextProvider from './Contexts/ThemeContextProvider';
 import Login from './Components/Form';
-import {Provider} from 'react-redux';
-import {store} from './Store';
-
-const App = () => {
-    return (
-        <Router>
-        <Provider store={store}>
-        <ThemeContextProvider>
-            <div className="App">
-                <ul style={{ display: "flex", justifyContent: "space-around", width: "20%", border: "3px solid red", color: "white", backgroundColor: "black" }} >
-                    <li><Link to="/home">Home</Link> </li>
-                   
-                </ul>
-                <Switch>
-                    <Route exact path='/'>
-                        <h1>Landing Page</h1>
-                    </Route>
-
-                    <Route path='/login'>
-                       <Login/>
-                    </Route>
-
-                    <Route path='/home'>
-                       <HomePage/>
-                    </Route>
-                    {
-                    // <Route exact path='/products'>
-                    //     <UserList />
-                    // </Route>
-
-                    // <Route path='/users/:id'>
-                    //     <FullUser />
-                    // </Route>
-                    }
-                </Switch>
-            </div>
-            </ThemeContextProvider>
-            </Provider>
-        </Router>
-    )
-}
+import { Provider } from 'react-redux';
+import { store } from './Store';
+import { Component } from 'react';
+import { Formik } from 'formik';
 
 
+const Basic = () => (
+   <div>
+     <h1>Anywhere in your app!</h1>
+     <Formik
+       initialValues={{ email: '', password: '' }}
+       validate={values => {
+         const errors = {};
+         if (!values.email) {
+           errors.email = 'Required';
+         } else if (
+           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+         ) {
+           errors.email = 'Invalid email address';
+         }
+         return errors;
+       }}
+       onSubmit={(values, { setSubmitting }) => {
+         setTimeout(() => {
+           alert(JSON.stringify(values, null, 2));
+           setSubmitting(false);
+         }, 400);
+       }}
+     >
+       {({
+         values,
+         errors,
+         touched,
+         handleChange,
+         handleBlur,
+         handleSubmit,
+         isSubmitting,
+         /* and other goodies */
+       }) => (
+         <form onSubmit={handleSubmit}>
+           <input
+             type="email"
+             name="email"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.email}
+           />
+           {errors.email && touched.email && errors.email}
+           <input
+             type="password"
+             name="password"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.password}
+           />
+           {errors.password && touched.password && errors.password}
+           <button type="submit" disabled={isSubmitting}>
+             Submit
+           </button>
+         </form>
+       )}
+     </Formik>
+   </div>
+ );
+ 
+ export default Basic;
 
-export default App;
 
+
+ const Colummns=()=>
+ {
+     return( 
+     <div>
+     <td>fvf</td>
+     <td>fvf</td>
+     <td>fvf</td>
+     <td>fvf</td>
+     <td>fvf</td>
+     </div>
+     )
+
+ }
